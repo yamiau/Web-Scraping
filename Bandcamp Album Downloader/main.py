dc67dd9fc06e08af89530fea2e8c8796a3170d83
Yami <mu.meiraaz@gmail.com>
2023-10-28 22:12:44 -0300

    Implement download function
    Also refactored imports

--- Bandcamp Album Downloader/main.py
+++ Bandcamp Album Downloader/main.py
@@ -1,65 +1,63 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-import ctypes
-import codecs
-import re
-import requests as rq
+import urllib.request as rq
+import requests as rqs
 from bs4 import BeautifulSoup as bs
 import os
+# import eyeD3
 import tkinter
 
+url = 'https://littletybee.bandcamp.com/album/little-tybee'
+save_path = 'C:\\Users\\Yami\\Desktop\\'
+# url = input(str('Album URL: '))
+# save_path = input(str('Save path (album folder will be created): '))
 
-url = "https://littletybee.bandcamp.com/album/little-tybee"
 
-#Set up BeautifulSoup
-
-album_page = rq.get(url)
+# Set up BeautifulSoup
+album_page = rqs.get(url)
 soup = bs(album_page.text, 'html.parser')
 
-#Album Date
+# Album Date
 album_date = soup.find('div', class_='tralbumData tralbum-credits').get_text().split('\n')
 album_date = album_date[2].split(' ')[-1]
 print(album_date)
 
-#Album Title
+# Album Title
 album_title = soup.find('h2', class_='trackTitle').get_text().split('\n')
 album_title = str(album_title[1].split(' ')[-2]) + ' ' + str(album_title[1].split(' ')[-1])
 print(album_title)
 
-#Artist
+# Artist
 artist = soup.find_all('span', class_='title')[1].get_text()
 print(artist)
 
-#Cover Image
+# Cover Image
 cover = soup.find('a', class_='popupImage').find('img')['src']
 print(cover)
 
-#Tracklist
+# Tracklist
 track_table = soup.find('table', class_='track_list track_table')
 
 track_list = track_table.find_all('div', class_='track_number secondaryText')
 nums = [i.get_text() for i in track_list]
 
-#Track Titles
+# Track Titles
 track_titles = track_table.find_all('span', class_='track-title')
 titles = [i.get_text() for i in track_titles]
 
-#Track Lengths
+# Track Lengths
 track_lengths = track_table.find_all('span', class_='time secondaryText')
 track_lengths = [i.get_text().replace(' ', '') for i in track_lengths]
 lengths = [i.replace('\n', '') for i in track_lengths]
 
-#Track Files
+# Track Files
 files = []
 
-#Set up WebDriver
+# Set up WebDriver
 options = webdriver.ChromeOptions()
-# options.add_argument("--headless=new")
+options.add_argument("--headless=new")
 options.add_argument('--incognito')
 options.add_argument('--mute-audio')
 options.add_experimental_option('detach', True)
@@ -75,11 +73,34 @@ for i in range(len(track_list)):
 driver.close()
 driver.quit()
 
-#Mount Album Dictionary
+# Mount Album Dictionary
 tracks_info = dict(zip(nums, map(list, zip(*(map(str, e) for e in (titles, lengths, files))))))
 
-for i in tracks_info.items():
-    print(i)
+# Download
+opener = rq.build_opener()
+opener.addheaders = [('User-agent', 'Chrome/83.0.4103.97')]
+rq.install_opener(opener)
+
+fullpath = save_path + f'[{album_date}] {album_title} ({artist})' + '\\'
+
+try:
+    os.mkdir(fullpath)
+except Exception as e:
+    raise e
+finally:
+    cover_extension = cover[-4:]
+    rq.urlretrieve(cover, fullpath + 'cover' + cover_extension)
+
+
+def save_from_url(title, url):
+    rq.urlretrieve(url, fullpath + title)
+
+
+for key, value in tracks_info.items():
+    title = key + ' ' + value[0] + '.mp3'
+    save_from_url(title, value[2])
+
+
 
 
 
